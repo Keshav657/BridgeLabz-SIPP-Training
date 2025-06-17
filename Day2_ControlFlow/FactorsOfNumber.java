2589d402e2f25eb9311d9d4450bf5e9e5780b0d5
Keshav657 <keshavagrawal304@gmail.com>
2025-06-17 11:53:02 +0530

    [keshav] to find factor of number

---
+++ Day2_ControlFlow/FactorsOfNumber.java
@@ -0,0 +1,25 @@
+package Day2_ControlFlow;
+
+import java.util.Scanner;
+
+public class FactorsOfNumber {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a positive integer: ");
+        int number = sc.nextInt();
+
+        if (number <= 0) {
+            System.out.println("Please enter a positive integer.");
+        } else {
+            System.out.print("Factors of " + number + " are: ");
+            int counter = 1;
+            while (counter <= number) {
+                if (number % counter == 0) {
+                    System.out.print(counter + " ");
+                }
+                counter++;
+            }
+            System.out.println();
+        }
+    }
+}
