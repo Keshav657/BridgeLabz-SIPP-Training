f36a7a8ffcca6191ac6b7024dce98212d44dc969
Keshav657 <keshavagrawal304@gmail.com>
2025-06-17 10:21:26 +0530

    [keshav] check number type

---
+++ Day2_ControlFlow/CheckNumberType.java
@@ -0,0 +1,19 @@
+package Day2_ControlFlow;
+
+import java.util.Scanner;
+
+public class CheckNumberType {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a number: ");
+        int number = sc.nextInt();
+
+        if (number > 0) {
+            System.out.println("positive");
+        } else if (number < 0) {
+            System.out.println("negative");
+        } else {
+            System.out.println("zero");
+        }
+    }
+}
