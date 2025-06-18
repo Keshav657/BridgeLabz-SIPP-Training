365f3e8b4fb6a96cbb59736c9f3ca08be15d3bbd
Keshav657 <keshavagrawal304@gmail.com>
2025-06-18 10:09:26 +0530

    [keshav] find positive negative or zero

---
+++ Day3_Arrays/PosNegZero.java
@@ -0,0 +1,40 @@
+package Day3_Arrays;
+
+import java.util.Scanner;
+
+public class PosNegZero {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int[] numbers = new int[5];
+
+        System.out.println("Enter 5 numbers:");
+        for (int i = 0; i < numbers.length; i++) {
+            numbers[i] = sc.nextInt();
+        }
+
+        for (int num : numbers) {
+            if (num > 0) {
+                System.out.print(num + " is positive and ");
+                if (num % 2 == 0) {
+                    System.out.println("even.");
+                } else {
+                    System.out.println("odd.");
+                }
+            } else if (num < 0) {
+                System.out.println(num + " is negative.");
+            } else {
+                System.out.println(num + " is zero.");
+            }
+        }
+
+        if (numbers[0] == numbers[numbers.length - 1]) {
+            System.out.println("First and last elements are equal.");
+        } else if (numbers[0] > numbers[numbers.length - 1]) {
+            System.out.println("First element is greater than last element.");
+        } else {
+            System.out.println("First element is less than last element.");
+        }
+
+        sc.close();
+    }
+}
