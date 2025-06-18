b477b3a8c4f1fba23ff1be26a025bd48069fd5e3
Keshav657 <keshavagrawal304@gmail.com>
2025-06-18 10:31:31 +0530

    [keshav] find Sam average

--- Day1_ProgrammingElements/Q2_SamAverage.java
+++ Day1_ProgrammingElements/Q2_SamAverage.java
@@ -1,11 +1,13 @@
 package Day1_ProgrammingElements;
 
+import java.util.Scanner;
 public class Q2_SamAverage {
     public static void main(String[] args) {
-        int maths = 94;
-        int physics = 95;
-        int chemistry = 96;
-
+        Scanner sc = new Scanner(System.in);
+        int maths = sc.nextInt();
+        int physics = sc.nextInt();
+        int chemistry = sc.nextInt();
+        
         double average = (maths + physics + chemistry) / 3.0;
 
         System.out.printf("Sam’s average mark in PCM is %.2f\n", average);
