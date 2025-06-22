e312cb34196f8b8e46d80db84c8329718237d02e
Keshav657 <keshavagrawal304@gmail.com>
2025-06-22 16:25:32 +0530

    [keshav] divide chocolates equally in users

---
+++ Day4_Methods/DivideChocolates.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class DivideChocolates {
+
+    public static int[] findRemainderAndQuotient(int number, int divisor) {
+        int quotient = number / divisor;
+        int remainder = number % divisor;
+        return new int[]{quotient, remainder};
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter number of chocolates: ");
+        int chocolates = scanner.nextInt();
+        System.out.print("Enter number of children: ");
+        int children = scanner.nextInt();
+        int[] result = findRemainderAndQuotient(chocolates, children);
+        System.out.println("Chocolates per child: " + result[0]);
+        System.out.println("Remaining chocolates: " + result[1]);
+        scanner.close();
+    }
+}
