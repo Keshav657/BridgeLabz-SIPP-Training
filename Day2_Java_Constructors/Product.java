1b1ff6a4ba7ced5d882645e1a3898470111f8fa8
Keshav657 <keshavagrawal304@gmail.com>
2025-07-02 12:25:29 +0530

    [keshav] display details of product and show total number of product created

---
+++ Day2_Java_Constructors/Product.java
@@ -0,0 +1,30 @@
+package Day2_Java_Constructors;
+
+class Product {
+    String productName;
+    double price;
+    static int totalProducts = 0;
+
+    Product(String productName, double price) {
+        this.productName = productName;
+        this.price = price;
+        totalProducts++;
+    }
+
+    void displayProductDetails() {
+        System.out.println("Product Name: " + productName + ", Price: $" + price);
+    }
+
+    static void displayTotalProducts() {
+        System.out.println("Total Products: " + totalProducts);
+    }
+
+    public static void main(String[] args) {
+        Product p1 = new Product("Laptop", 1200.50);
+        Product p2 = new Product("Phone", 800.00);
+        p1.displayProductDetails();
+        p2.displayProductDetails();
+        Product.displayTotalProducts();
+    }
+}
+
