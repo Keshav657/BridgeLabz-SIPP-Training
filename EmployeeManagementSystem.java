2446aee1e0aaee25cae641147878c4331cad02ae
Keshav Agrawal <149050565+Keshav657@users.noreply.github.com>
2025-08-04 12:00:15 +0530

    [keshav] Create EmployeeManagementSystem.java

---
+++ EmployeeManagementSystem.java
@@ -0,0 +1,79 @@
+package Inheritance;
+
+// Base class
+class Employee {
+    protected String name;
+    protected int id;
+    protected double salary;
+
+    public Employee(String name, int id, double salary) {
+        this.name = name;
+        this.id = id;
+        this.salary = salary;
+    }
+
+    public void displayDetails() {
+        System.out.println("Name: " + name + ", ID: " + id + ", Salary: " + salary);
+    }
+}
+
+// Manager subclass
+class Manager extends Employee {
+    private int teamSize;
+
+    public Manager(String name, int id, double salary, int teamSize) {
+        super(name, id, salary);
+        this.teamSize = teamSize;
+    }
+
+    @Override
+    public void displayDetails() {
+        super.displayDetails();
+        System.out.println("Role: Manager, Team Size: " + teamSize);
+    }
+}
+
+// Developer subclass
+class Developer extends Employee {
+    private String programmingLanguage;
+
+    public Developer(String name, int id, double salary, String programmingLanguage) {
+        super(name, id, salary);
+        this.programmingLanguage = programmingLanguage;
+    }
+
+    @Override
+    public void displayDetails() {
+        super.displayDetails();
+        System.out.println("Role: Developer, Programming Language: " + programmingLanguage);
+    }
+}
+
+// Intern subclass
+class Intern extends Employee {
+    private String university;
+
+    public Intern(String name, int id, double salary, String university) {
+        super(name, id, salary);
+        this.university = university;
+    }
+
+    @Override
+    public void displayDetails() {
+        super.displayDetails();
+        System.out.println("Role: Intern, University: " + university);
+    }
+}
+
+// Demo
+public class EmployeeManagementSystem {
+    public static void main(String[] args) {
+        Employee e1 = new Manager("Alice", 101, 90000, 5);
+        Employee e2 = new Developer("Bob", 102, 70000, "Java");
+        Employee e3 = new Intern("Charlie", 103, 20000, "XYZ University");
+
+        e1.displayDetails();
+        e2.displayDetails();
+        e3.displayDetails();
+    }
+}
