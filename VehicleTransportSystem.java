6d7c12d513ac077a4d15689b86f577d096e2143e
Keshav Agrawal <149050565+Keshav657@users.noreply.github.com>
2025-08-04 12:01:57 +0530

    [keshav] Create VehicleTransportSystem.java

---
+++ VehicleTransportSystem.java
@@ -0,0 +1,79 @@
+package Inheritance;
+
+// Superclass
+class Vehicle {
+    protected int maxSpeed;
+    protected String fuelType;
+
+    public Vehicle(int maxSpeed, String fuelType) {
+        this.maxSpeed = maxSpeed;
+        this.fuelType = fuelType;
+    }
+
+    public void displayInfo() {
+        System.out.println("Max Speed: " + maxSpeed + " km/h, Fuel Type: " + fuelType);
+    }
+}
+
+// Car subclass
+class Car extends Vehicle {
+    private int seatCapacity;
+
+    public Car(int maxSpeed, String fuelType, int seatCapacity) {
+        super(maxSpeed, fuelType);
+        this.seatCapacity = seatCapacity;
+    }
+
+    @Override
+    public void displayInfo() {
+        super.displayInfo();
+        System.out.println("Type: Car, Seat Capacity: " + seatCapacity);
+    }
+}
+
+// Truck subclass
+class Truck extends Vehicle {
+    private double loadCapacity;
+
+    public Truck(int maxSpeed, String fuelType, double loadCapacity) {
+        super(maxSpeed, fuelType);
+        this.loadCapacity = loadCapacity;
+    }
+
+    @Override
+    public void displayInfo() {
+        super.displayInfo();
+        System.out.println("Type: Truck, Load Capacity: " + loadCapacity + " tons");
+    }
+}
+
+// Motorcycle subclass
+class Motorcycle extends Vehicle {
+    private boolean hasCarrier;
+
+    public Motorcycle(int maxSpeed, String fuelType, boolean hasCarrier) {
+        super(maxSpeed, fuelType);
+        this.hasCarrier = hasCarrier;
+    }
+
+    @Override
+    public void displayInfo() {
+        super.displayInfo();
+        System.out.println("Type: Motorcycle, Has Carrier: " + (hasCarrier ? "Yes" : "No"));
+    }
+}
+
+// Demo
+public class VehicleTransportSystem {
+    public static void main(String[] args) {
+        Vehicle[] vehicles = new Vehicle[3];
+        vehicles[0] = new Car(180, "Petrol", 5);
+        vehicles[1] = new Truck(120, "Diesel", 10.5);
+        vehicles[2] = new Motorcycle(150, "Petrol", true);
+
+        for (Vehicle v : vehicles) {
+            v.displayInfo();
+            System.out.println("---");
+        }
+    }
+}
